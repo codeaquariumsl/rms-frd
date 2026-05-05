de775aeb2d4af794157be56b471155b3d2b88260
sadeepaZincat <sadeepa.zincat@gmail.com>
2026-05-06 00:09:34 +0530

    implemented a Reverse Proxy fix using Next.js Rewrites.

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,15 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async rewrites() {
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL?.replace(/\/$/, '') || 'http://localhost:5000/api/v1';
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${apiUrl}/:path*`,
+      },
+    ]
+  },
 }
 
 export default nextConfig
